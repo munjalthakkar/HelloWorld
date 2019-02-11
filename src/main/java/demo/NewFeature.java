983cb61012442f6081a57c02c9cfe4d65280310a
munjalthakkar <munjal.thakkar@gmail.com>
2019-02-11 17:15:37 -0500

    comment added to the new feature based on the code review feedback

--- src/main/java/demo/NewFeature.java
+++ src/main/java/demo/NewFeature.java
@@ -3,6 +3,7 @@ package demo;
 public class NewFeature {
 
     public static void main(String[] args) {
+        //comment for the new feature
         System.out.println("New Feature class");
     }
 }
