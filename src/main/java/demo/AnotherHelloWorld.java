f4373a7f3aebcf474bf82828fb5b4f80437121b1
munjalthakkar <munjal.thakkar@gmail.com>
2019-02-11 17:06:06 -0500

    New File added

---
+++ src/main/java/demo/AnotherHelloWorld.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class AnotherHelloWorld {
+
+    public static void main(String[] args) {
+
+        // Added comment
+        System.out.println("Hello GitHub!");
+    }
+}
